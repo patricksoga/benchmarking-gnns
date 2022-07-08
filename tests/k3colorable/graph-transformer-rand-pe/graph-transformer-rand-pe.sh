3561739e6ea613c8a39d2ddde70745bc25156491
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-07-08 15:54:51 -0400

    tests

--- tests/k3colorable/graph-transformer-rand-pe/graph-transformer-rand-pe.sh
+++ tests/k3colorable/graph-transformer-rand-pe/graph-transformer-rand-pe.sh
@@ -7,5 +7,5 @@
 encdims=(0 5 10 15 20 30)
 
 conda activate gnn
-cd ../../
+cd ../../../
 python3 main_K3Colorable_graph_classification.py --config configs/K3Colorable_graph_classification_GraphTransformer_randPE_500k_sparse_graph_BN.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]}
