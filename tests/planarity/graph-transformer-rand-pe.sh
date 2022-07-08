9695b1af5751ebe86d21432d7e9ea1e78a7bc945
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-07-08 01:53:40 -0400

    rand pe

--- tests/planarity/graph-transformer-rand-pe.sh
+++ tests/planarity/graph-transformer-rand-pe.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#$ -N gtn-planarity-500k-learn-pe
+#$ -N gtn-planarity-500k-rand-pe
 #$ -q gpu
 #$ -l gpu_card=1
 #$ -t 1-5:1
 
-encdims=(0 20 40 80 100 120)
+encdims=(0 5 10 15 20 30)
 
 conda activate gnn
 cd ../../
-python3 main_PLANARITY_graph_classification.py --config configs/PLANARITY_graph_classification_GraphTransformer_learnedPE_500k_sparse_graph_BN.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]}
+python3 main_PLANARITY_graph_classification.py --config configs/PLANARITY_graph_classification_GraphTransformer_randPE_500k_sparse_graph_BN.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]}
