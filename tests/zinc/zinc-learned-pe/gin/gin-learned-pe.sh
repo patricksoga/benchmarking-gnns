8f1c53235e27091667382bf3568da764a327cf45
patricksoga <psoga@nd.edu>
2022-07-08 21:18:30 -0700

    up dir

--- tests/zinc/zinc-learned-pe/gin/gin-learned-pe.sh
+++ tests/zinc/zinc-learned-pe/gin/gin-learned-pe.sh
@@ -7,5 +7,5 @@
 encdims=(0 5 10 15 20 30)
 
 conda activate gnn
-cd ../../
+cd ../../../../
 python3 main_molecules_graph_regression.py --config configs/molecules_graph_regression_GIN_ZINC_learnedPE_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID}
