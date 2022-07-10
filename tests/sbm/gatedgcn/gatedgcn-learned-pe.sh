0f4d3fae724cf264125787fac6379bd02463abfa
patricksoga <psoga@nd.edu>
2022-07-09 19:19:40 -0700

    gatedgcn and gin scripts for sbm

---
+++ tests/sbm/gatedgcn/gatedgcn-learned-pe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -N gatedgcn-pattern-100k-learned-pe
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-5:1
+
+encdims=(0 5 10 15 20 30)
+
+conda activate gnn
+cd ../../../../
+python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GatedGCN_PATTERN_learnedPE_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID}
