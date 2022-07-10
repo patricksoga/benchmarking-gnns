90791a863b99345bd4c4a0192feece6f7caa517e
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-07-09 22:24:16 -0400

    waiting on sbm, zinc edge feat, and gtn results for all sbm zinc and cycle sets

--- tests/sbm/gin/gin-learned-pe.sh
+++ tests/sbm/gin/gin-learned-pe.sh
@@ -7,5 +7,5 @@
 encdims=(0 5 10 15 20 30)
 
 conda activate gnn
-cd ../../../../
+cd ../../../
 python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GIN_PATTERN_learnedPE_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID}
--- tests/sbm/gtn/gtn-lape/gtn-lape.sh
+++ tests/sbm/gtn/gtn-lape/gtn-lape.sh
@@ -4,5 +4,5 @@
 #$ -l gpu_card=1
 
 conda activate gnn
-cd ../../../
+cd ../../../../
 python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GraphTransformer_PATTERN_PE_500k.json
