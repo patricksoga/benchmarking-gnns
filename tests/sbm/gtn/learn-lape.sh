900b190d2deb3d2780f0fb77fe107f9c3e0fe393
patricksoga <psoga@nd.edu>
2022-07-10 22:27:40 -0700

    learn-lape script

---
+++ tests/sbm/gtn/learn-lape.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -N gtn-pattern-500k-learned-lape-pe
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-5:1
+
+encdims=(0 10 20 30 40 50)
+
+conda activate gnn
+cd ../../../../
+python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GraphTransformer_PATTERN_learnedlapePE_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --batch_size 4
