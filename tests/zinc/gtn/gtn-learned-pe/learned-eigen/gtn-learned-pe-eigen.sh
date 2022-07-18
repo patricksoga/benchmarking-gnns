7b84d55f374c87bc89803f48fa032952cf798463
patricksoga <psoga@nd.edu>
2022-07-18 09:02:02 -0700

    gtn zinc

--- tests/zinc/gtn/gtn-learned-pe/learned-eigen/gtn-learned-pe-eigen.sh
+++ tests/zinc/gtn/gtn-learned-pe/learned-eigen/gtn-learned-pe-eigen.sh
@@ -7,5 +7,5 @@
 encdims=(0 5 10 20)
 
 conda activate gnn
-cd ../../../../
-python3 main_molecules_graph_regression.py --config configs/molecules_graph_regression_GraphTransformer_ZINC_learnedPE_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --edge_feat --matrix_type E
+cd ../../../../../
+python3 main_molecules_graph_regression.py --config configs/molecules_graph_regression_GraphTransformer_ZINC_learnedPE_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --edge_feat --in_feat_dropout 0.4 --batch_size 4 --matrix_type E
