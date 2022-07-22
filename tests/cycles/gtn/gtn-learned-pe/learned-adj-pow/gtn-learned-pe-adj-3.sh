f6cdf7a19e7cbb5090b2ce1f84ff5f445c8a65d6
patricksoga <psoga@nd.edu>
2022-07-22 03:14:59 -0700

    add log fsync

--- tests/cycles/gtn/gtn-learned-pe/learned-adj-pow/gtn-learned-pe-adj-3.sh
+++ tests/cycles/gtn/gtn-learned-pe/learned-adj-pow/gtn-learned-pe-adj-3.sh
@@ -5,9 +5,9 @@
 #$ -t 1-2:1
 
 encdims=(0 15 20)
+touch k_3_${SGE_TASK_ID}_DEBUG_.log
+fsync -d 10 k_3_${SGE_TASK_ID}_DEBUG_.log &
 
 conda activate gnn
 cd ../../../../
-touch logs/gtn-cycles-500k-learned-pe-adj-k_3_${SGE_TASK_ID}_DEBUG_.log
-fsync -d 10 logs/logs/gtn-cycles-500k-learned-pe-adj-k_3_${SGE_TASK_ID}_DEBUG_.log &
 python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_learnedPE_CYCLES_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --pow_of_mat 3
