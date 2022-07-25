c6022a74e77e8d4d9fc4174782441e5f57d3c631
patricksoga <psoga@nd.edu>
2022-07-25 15:10:02 -0700

    dropout

--- tests/cycles/gtn/gtn-adj/gtn-adj.sh
+++ tests/cycles/gtn/gtn-adj/gtn-adj.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#$ -N gtn-cycles-500k-adj
+#$ -N gtn-cycles-500k-adj-dropout
 #$ -q gpu
 #$ -l gpu_card=1
 
-fname=$(pwd)/cycles_adj_${SGE_TASK_ID}_DEBUG_.log
+fname=$(pwd)/cycles_adj_${SGE_TASK_ID}_dropout_DEBUG_.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd ../../../../
-python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_PE_CYCLES_500k.json --batch_size 2 --job_num ${SGE_TASK_ID} --log_file $fname --batch_size 2 --pos_enc_dim 20 --adj_enc
+python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_PE_CYCLES_500k.json --batch_size 2 --job_num ${SGE_TASK_ID} --log_file $fname --batch_size 2 --pos_enc_dim 20 --adj_enc --in_feat_dropout 0.8
