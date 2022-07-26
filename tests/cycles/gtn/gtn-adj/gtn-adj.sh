eeae15a43a4619f681d3026029c3cb2890ce2126
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-07-26 14:40:23 -0400

    results

--- tests/cycles/gtn/gtn-adj/gtn-adj.sh
+++ tests/cycles/gtn/gtn-adj/gtn-adj.sh
@@ -3,10 +3,10 @@
 #$ -q gpu
 #$ -l gpu_card=1
 
-fname=$(pwd)/cycles_adj_${SGE_TASK_ID}_dropout_0.5_DEBUG_.log
+fname=$(pwd)/cycles_adj_${SGE_TASK_ID}_dropout_0.25_DEBUG_.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd ../../../../
-python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_PE_CYCLES_500k.json --batch_size 2 --job_num ${SGE_TASK_ID} --log_file $fname --batch_size 2 --pos_enc_dim 20 --adj_enc --in_feat_dropout 0.5
+python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_PE_CYCLES_500k.json --batch_size 2 --job_num ${SGE_TASK_ID} --log_file $fname --batch_size 2 --pos_enc_dim 20 --adj_enc --in_feat_dropout 0.25
--- tests/sbm/gtn/gtn-adj/gtn-adj.sh
+++ tests/sbm/gtn/gtn-adj/gtn-adj.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#$ -N gtn-pattern-500k-adj-dropout
+#$ -N gtn-pattern-500k-adj-dropout-layernorm-3layers
 #$ -q gpu
 #$ -l gpu_card=1
 
-fname=$(pwd)/pattern_adj_${SGE_TASK_ID}_dropout_DEBUG_.log
+fname=$(pwd)/pattern_adj_${SGE_TASK_ID}_dropout_layernorm_3layers_DEBUG_.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd ../../../../
-python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GraphTransformer_PATTERN_PE_500k.json --batch_size 4 --job_num ${SGE_TASK_ID} --log_file $fname --pos_enc_dim 20 --adj_enc --in_feat_dropout 0.5 --L 8
+python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GraphTransformer_PATTERN_PE_500k.json --batch_size 4 --job_num ${SGE_TASK_ID} --log_file $fname --pos_enc_dim 20 --adj_enc --in_feat_dropout 0.4 --L 3 --layer_norm True
