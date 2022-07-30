168d6db6c11c2c8991fc3baf30f6baaa8d38d025
patricksoga <psoga@nd.edu>
2022-07-29 22:03:25 -0700

    new tests, gin and gatedgcn

---
+++ tests/cycles/gatedgcn/learned-adj/gatedgcn-learned-adj-repeat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -N gatedgcn-cycles-100k-learned-pe-repeat-dim
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-2:1
+
+encdims=(0 10 15)
+fname=$(pwd)/repeat_${SGE_TASK_ID}_DEBUG_.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd ../../../../
+python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GatedGCN_learnedPE_CYCLES_100k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname --batch_size 10
