3d40f49c0d756e477e791faa423a2f3626a8bce9
patricksoga <psoga@nd.edu>
2022-08-02 13:16:58 -0700

    tests

--- tests/cycles/gatedgcn/learned-adj/gatedgcn-learned-adj-repeat.sh
+++ tests/cycles/gatedgcn/learned-adj/gatedgcn-learned-adj-repeat.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#$ -N gatedgcn-cycles-100k-learned-pe-repeat-dim-dropout
+#$ -N gatedgcn-cycles-100k-learned-pe-b25
 #$ -q gpu
 #$ -l gpu_card=1
-#$ -t 1-5:1
+#$ -t 1-2:1
 
-encdims=(0 10 11 12 13 14)
-fname=$(pwd)/repeat_dout_0.25_${SGE_TASK_ID}_DEBUG_.log
+encdims=(0 10 15)
+fname=$(pwd)/repeat_b25_${SGE_TASK_ID}_DEBUG_.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd ../../../../
-python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GatedGCN_learnedPE_CYCLES_100k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname --batch_size 10 --in_feat_dropout 0.25
+python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GatedGCN_learnedPE_CYCLES_100k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname
