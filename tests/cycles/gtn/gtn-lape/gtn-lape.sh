3395c9e318bad7f3427dc8a46173aa4e4ace78cd
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-08-03 17:18:10 -0400

    results

--- tests/cycles/gtn/gtn-lape/gtn-lape.sh
+++ tests/cycles/gtn/gtn-lape/gtn-lape.sh
@@ -9,4 +9,4 @@ fsync -d 10 $fname &
 
 conda activate gnn
 cd ../../../../
-python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_PE_CYCLES_500k.json --log_file $fname --batch_size 25
+python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_PE_CYCLES_500k.json --log_file $fname --batch_size 25 --job_num 1
