7a93f2acaa5aece54ccd622410ee5141c504cdbf
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-08-03 02:57:30 -0400

    results

--- tests/cycles/gatedgcn/learned-adj/dout-0.25/gatedgcn-learned-adj-repeat.sh
+++ tests/cycles/gatedgcn/learned-adj/dout-0.25/gatedgcn-learned-adj-repeat.sh
@@ -10,5 +10,5 @@ touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
-cd ../../../../
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GatedGCN_learnedPE_CYCLES_100k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname --dropout 0.25
--- tests/cycles/gtn/gtn-learned-pe/learned-adj/baseline/gtn-learned-pe-repeat-dim.sh
+++ tests/cycles/gtn/gtn-learned-pe/learned-adj/baseline/gtn-learned-pe-repeat-dim.sh
@@ -4,11 +4,11 @@
 #$ -l gpu_card=1
 #$ -t 1-5:1
 
-encdims=(0 10 11 12 13 14)
+encdims=(0 10 11 12 13 14 15)
 fname=$(pwd)/repeat_${SGE_TASK_ID}_DEBUG_.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
-cd ../../../../../../
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_learnedPE_CYCLES_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname
