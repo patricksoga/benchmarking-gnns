de88331245102d70cd0e41991d70c12e78b94e78
patricksoga <psoga@nd.edu>
2022-08-02 23:28:20 -0700

    update

--- tests/sbm/gatedgcn/learned-pe/learned-adj/b16/learned-adj-b16.sh
+++ tests/sbm/gatedgcn/learned-pe/learned-adj/b16/learned-adj-b16.sh
@@ -3,11 +3,10 @@
 #$ -q gpu
 #$ -l gpu_card=1
 
-encdims=(0 10)
-fname=$(pwd)/repeat_b16_${SGE_TASK_ID}_DEBUG_${encdims[${SGE_TASK_ID}]}.log
+fname=$(pwd)/repeat_b16_DEBUG_.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd ../../../../../../
-python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GatedGCN_PATTERN_learnedPE_100k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname --batch_size 16
+python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GatedGCN_PATTERN_learnedPE_100k.json --pos_enc_dim 10 --job_num 1 --log_file $fname --batch_size 16
