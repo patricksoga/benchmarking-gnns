287141fc77d5ee581ddaa5e67ff133bc62544bd5
patricksoga <psoga@nd.edu>
2022-08-03 14:10:58 -0700

    gtn

--- tests/cycles/gtn/gtn-lape/gtn-lape.sh
+++ tests/cycles/gtn/gtn-lape/gtn-lape.sh
@@ -3,10 +3,10 @@
 #$ -q gpu
 #$ -l gpu_card=1
 
-fname=$(pwd)/lape_DEBUG_.log
+fname=$(pwd)/lape_DEBUG_b25.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd ../../../../
-python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_PE_CYCLES_500k.json --log_file $fname
+python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_PE_CYCLES_500k.json --log_file $fname --batch_size 25
