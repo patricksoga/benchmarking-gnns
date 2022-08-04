8f3cb82b27bd7a660fcccd45837f962b67a91cc6
patricksoga <psoga@nd.edu>
2022-08-03 22:41:48 -0700

    layernorm batch 25

---
+++ tests/cycles/gtn/gtn-learned-pe/learned-adj/layernorm-b25/gtn-learned-pe-repeat-dim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -N gtn-cycles-500k-learned-pe-repeat-dim
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-4:1
+
+encdims=(0 12 13 14 15)
+fname=$(pwd)/repeat_${SGE_TASK_ID}_DEBUG_.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_learnedPE_CYCLES_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname --layer_norm True --batch_norm False --batch_size 25
