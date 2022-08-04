d07a300d74adbfccc77710d4adecfa7e2a73d996
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-08-03 22:13:08 -0400

    results

--- tests/sbm/gtn/gtn-learned-pe/learned-adj/no-dropout/gtn-learned-pe-repeat-dim-dropout.sh
+++ tests/sbm/gtn/gtn-learned-pe/learned-adj/no-dropout/gtn-learned-pe-repeat-dim-dropout.sh
@@ -4,11 +4,11 @@
 #$ -l gpu_card=1
 #$ -t 1-4:1
 
-encdims=(0 10 12 15 20)
+encdims=(0 5 10 12 15 20)
 fname=$(pwd)/repeat_nodout_${SGE_TASK_ID}_DEBUG_${encdims[${SGE_TASK_ID}]}.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
-cd ../../../../../../
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GraphTransformer_PATTERN_learnedPE_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --batch_size 1 --log_file $fname
