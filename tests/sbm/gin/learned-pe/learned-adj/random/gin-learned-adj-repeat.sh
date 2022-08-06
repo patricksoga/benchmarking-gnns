3af5a9195fc41aea8dc3e50f1204fbf859891d31
patricksoga <psoga@nd.edu>
2022-08-05 21:49:51 -0700

    new tests

---
+++ tests/sbm/gin/learned-pe/learned-adj/random/gin-learned-adj-repeat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -N gin-pattern-100k-rand-pe
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-5:1
+
+encdims=(0 64 128 256 512 1024)
+fname=$(pwd)/repeat_${SGE_TASK_ID}_DEBUG_${encdims[${SGE_TASK_ID}]}.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd ../../../../../
+python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GIN_PATTERN_randPE_100k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname
