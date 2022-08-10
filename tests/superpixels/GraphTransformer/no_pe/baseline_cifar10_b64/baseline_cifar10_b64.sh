c3cbfcdf4b41b8f0a2df12e649c5c6f3e9774ba4
patricksoga <psoga@nd.edu>
2022-08-09 19:05:34 -0700

    tests

---
+++ tests/superpixels/GraphTransformer/no_pe/baseline_cifar10_b64/baseline_cifar10_b64.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#$ -N GraphTransformer_CIFAR10_baseline_cifar10_b64
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-1:1
+
+pos_enc_dim=(0 1)
+fname=$(pwd)/baseline_cifar10_b64_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_superpixels_graph_classification.py --config tests/test-configs/GraphTransformer_superpixels_baseline_cifar10_b64.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+
+
+# {'dataset': 'CIFAR10',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'GraphTransformer',
+#  'net_params': {'L': 6,
+#                 'adj_enc': False,
+#                 'batch_norm': False,
+#                 'batch_size': 64,
+#                 'dataset': 'CIFAR10',
+#                 'dropout': 0.0,
+#                 'full_graph': False,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 80,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': True,
+#                 'learned_pos_enc': False,
+#                 'matrix_type': 'A',
+#                 'n_heads': 8,
+#                 'num_initials': 1,
+#                 'out_dim': 80,
+#                 'pos_enc': False,
+#                 'pos_enc_dim': 20,
+#                 'pow_of_mat': 1,
+#                 'rand_pos_enc': False,
+#                 'readout': 'sum',
+#                 'residual': True,
+#                 'self_loop': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/superpixels_graph_classification_baseline_cifar10_b64',
+#  'params': {'batch_size': 64,
+#             'epochs': 1000,
+#             'init_lr': 0.0005,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 10,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'seed': 41,
+#             'weight_decay': 0.0}}
+
