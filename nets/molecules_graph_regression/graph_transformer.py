55717c6529e5b9b00ef1bcc94cf40ce4638a3500
patricksoga <psoga@nd.edu>
2022-08-11 02:38:55 -0700

    give back node level embedding

--- nets/molecules_graph_regression/graph_transformer.py
+++ nets/molecules_graph_regression/graph_transformer.py
@@ -45,7 +45,7 @@ class GraphTransformerNet(nn.Module):
         self.MLP_layer = MLPReadout(out_dim, 1)   # 1 out dim since regression problem        
         
     def forward(self, g, h, e, pos_enc=None, h_wl_pos_enc=None):
-        # h = self.embedding_h(h)
+        h = self.embedding_h(h)
         # h = self.in_feat_dropout(h)
         h = self.pe_layer(g, h, pos_enc)
         h = self.in_feat_dropout(h)
