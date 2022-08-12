d80c6d293fbd364fc8985f23b6cbd14b83da2a7c
patricksoga <psoga@nd.edu>
2022-08-11 17:34:56 -0700

    cluster

---
+++ tests/SBMs_SBM_CLUSTER/GraphTransformer/no_pe/b2-baseline-lnorm/b2-baseline-lnorm.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#$ -N GraphTransformer_SBM_CLUSTER_b2-baseline-lnorm
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-1:1
+
+pos_enc_dim=(0 1)
+fname=$(pwd)/b2-baseline-lnorm_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_b2-baseline-lnorm.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname --init_lr 0.001
+
+
+# {'dataset': 'SBM_CLUSTER',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'GraphTransformer',
+#  'net_params': {'L': 6,
+#                 'adj_enc': False,
+#                 'batch_norm': False,
+#                 'batch_size': 2,
+#                 'dataset': 'SBM_CLUSTER',
+#                 'dropout': 0.0,
+#                 'full_graph': False,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 80,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': True,
+#                 'matrix_type': 'A',
+#                 'n_heads': 8,
+#                 'num_train_data': 7000,
+#                 'out_dim': 80,
+#                 'pos_enc': False,
+#                 'pos_enc_dim': 20,
+#                 'pow_of_mat': 1,
+#                 'readout': 'sum',
+#                 'residual': True,
+#                 'self_loop': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/SBMs_node_classification_b2-baseline-lnorm',
+#  'params': {'batch_size': 2,
+#             'epochs': 1000,
+#             'init_lr': 0.0005,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 10,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'seed': 41,
+#             'weight_decay': 0.0}}
+
