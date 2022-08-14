f16754cfe055f7d7a9a0712cb51e8355c214c70d
patricksoga <psoga@nd.edu>
2022-08-14 02:54:14 -0700

    tests

--- tests/SBMs_SBM_PATTERN/GraphTransformer/rand_pos_enc/b2-lnorm-alt/GraphTransformer_SBM_PATTERN_b2-lnorm-alt_8.sh
+++ tests/SBMs_SBM_PATTERN/GraphTransformer/rand_pos_enc/b2-lnorm-alt/GraphTransformer_SBM_PATTERN_b2-lnorm-alt_8.sh
@@ -10,4 +10,4 @@ fsync -d 10 $fname &
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_b2-lnorm-alt.json --job_num 8 --pos_enc_dim 8 --log_file $fname
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_PATTERN_b2-lnorm-alt.json --job_num 8 --pos_enc_dim 8 --log_file $fname
--- tests/SBMs_SBM_PATTERN/GraphTransformer/rand_pos_enc/b2-lnorm-alt/b2-lnorm-alt.sh
+++ tests/SBMs_SBM_PATTERN/GraphTransformer/rand_pos_enc/b2-lnorm-alt/b2-lnorm-alt.sh
@@ -12,7 +12,7 @@ fsync -d 10 $fname &
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_b2-lnorm-alt.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_PATTERN_b2-lnorm-alt.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
 
 
 # {'dataset': 'SBM_PATTERN',
