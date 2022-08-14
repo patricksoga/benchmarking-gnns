01470bc9228bdeea9d6f6a1b386126083ccd4351
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-08-13 23:15:32 -0400

    results

--- tests/molecules_AQSOL/GraphTransformer/learned_pos_enc/b64_lnorm_edge_feat_learned/b64_lnorm_edge_feat_learned.sh
+++ tests/molecules_AQSOL/GraphTransformer/learned_pos_enc/b64_lnorm_edge_feat_learned/b64_lnorm_edge_feat_learned.sh
@@ -12,7 +12,7 @@ fsync -d 10 $fname &
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_b64_lnorm_edge_feat_learned.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_b64_lnorm_edge_feat_learned.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname --batch_size 8
 
 
 # {'dataset': 'AQSOL',
