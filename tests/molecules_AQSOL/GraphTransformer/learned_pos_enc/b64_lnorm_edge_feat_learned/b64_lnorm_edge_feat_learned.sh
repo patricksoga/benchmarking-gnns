405af1a268e12309ed7b621d13b01424a3f25eef
patricksoga <psoga@nd.edu>
2022-08-13 19:43:40 -0700

    tests

--- tests/molecules_AQSOL/GraphTransformer/learned_pos_enc/b64_lnorm_edge_feat_learned/b64_lnorm_edge_feat_learned.sh
+++ tests/molecules_AQSOL/GraphTransformer/learned_pos_enc/b64_lnorm_edge_feat_learned/b64_lnorm_edge_feat_learned.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
-#$ -N GraphTransformer_SBM_CLUSTER_b2-lnorm-rand
+#$ -N GraphTransformer_AQSOL_b64_lnorm_edge_feat_learned
 #$ -q gpu
 #$ -l gpu_card=1
-#$ -t 1-5:1
+#$ -t 1-4:1
 
-pos_enc_dim=(0 16 32 40 64 80)
-fname=$(pwd)/b2-lnorm-rand_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_0.0005_DEBUG.log
+pos_enc_dim=(0 4 8 12 16)
+fname=$(pwd)/b64_lnorm_edge_feat_learned_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_GraphTransformer_CLUSTER_500k.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname --batch_norm False --layer_norm True --L 10 --readout "sum" --rand_pos_enc True --batch_size 2 --init_lr 0.0005 --pos_enc False
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_b64_lnorm_edge_feat_learned.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
 
 
-# {'dataset': 'SBM_CLUSTER',
+# {'dataset': 'AQSOL',
 #  'gpu': {'id': 0, 'use': True},
 #  'model': 'GraphTransformer',
 #  'net_params': {'L': 6,
 #                 'adj_enc': False,
 #                 'batch_norm': False,
-#                 'batch_size': 2,
-#                 'dataset': 'SBM_CLUSTER',
+#                 'batch_size': 64,
+#                 'dataset': 'AQSOL',
 #                 'dropout': 0.0,
+#                 'edge_feat': True,
 #                 'full_graph': False,
 #                 'gpu_id': 0,
 #                 'hidden_dim': 80,
 #                 'in_feat_dropout': 0.0,
 #                 'layer_norm': True,
+#                 'learned_pos_enc': True,
 #                 'matrix_type': 'A',
 #                 'n_heads': 8,
 #                 'num_train_data': 7000,
@@ -36,15 +38,15 @@ python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_G
 #                 'pos_enc': False,
 #                 'pos_enc_dim': 20,
 #                 'pow_of_mat': 1,
-#                 'rand_pos_enc': True,
+#                 'rand_pos_enc': False,
 #                 'readout': 'sum',
 #                 'residual': True,
 #                 'self_loop': False,
 #                 'wl_pos_enc': False},
-#  'out_dir': 'out/SBMs_node_classification_b2-lnorm-rand',
-#  'params': {'batch_size': 2,
+#  'out_dir': 'out/molecules_graph_regression_b64_lnorm_edge_feat_learned',
+#  'params': {'batch_size': 64,
 #             'epochs': 1000,
-#             'init_lr': 0.001,
+#             'init_lr': 0.0005,
 #             'lr_reduce_factor': 0.5,
 #             'lr_schedule_patience': 10,
 #             'max_time': 24,
@@ -53,3 +55,7 @@ python3 main_SBMs_node_classification.py --config configs/SBMs_node_clustering_G
 #             'seed': 41,
 #             'weight_decay': 0.0}}
 
+
+
+# Generated with command:
+#python3 configure_tests.py --config test-configs/GraphTransformer_molecules_b64_bnorm_edge_feat_rand.json --learned_pos_enc True --rand_pos_enc False --init_lr 0.0005 --edge_feat True --job_note b64_lnorm_edge_feat_learned --param_values 4 8 12 16 --varying_param pos_enc_dim --batch_norm False --layer_norm True
