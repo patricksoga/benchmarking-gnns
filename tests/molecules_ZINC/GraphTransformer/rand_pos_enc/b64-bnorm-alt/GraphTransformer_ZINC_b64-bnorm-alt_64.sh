a00db51c0d4428489bd847473d6afbab15ff84f0
patricksoga <psoga@nd.edu>
2022-08-14 02:14:14 -0700

    test scripts for qfunnel

---
+++ tests/molecules_ZINC/GraphTransformer/rand_pos_enc/b64-bnorm-alt/GraphTransformer_ZINC_b64-bnorm-alt_64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_ZINC_b64-bnorm-alt
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/64_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_b64-bnorm-alt.json --job_num 64 --pos_enc_dim 64 --log_file $fname
