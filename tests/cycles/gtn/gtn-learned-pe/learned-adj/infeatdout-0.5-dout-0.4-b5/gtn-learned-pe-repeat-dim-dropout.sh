212faeb38f9c6279a4996ec43a74f811bf4c5ead
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-08-14 01:48:53 -0400

    results

--- tests/cycles/gtn/gtn-learned-pe/learned-adj/infeatdout-0.5-dout-0.4-b5/gtn-learned-pe-repeat-dim-dropout.sh
+++ tests/cycles/gtn/gtn-learned-pe/learned-adj/infeatdout-0.5-dout-0.4-b5/gtn-learned-pe-repeat-dim-dropout.sh
@@ -11,4 +11,4 @@ fsync -d 10 $fname &
 
 conda activate gnn
 cd ../../../../../../
-python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_learnedPE_CYCLES_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname --in_feat_dropout 0.5 --dropout 0.4
+python3 main_CYCLES_graph_classification.py --config configs/CYCLES_graph_classification_GraphTransformer_learnedPE_CYCLES_500k.json --pos_enc_dim ${encdims[${SGE_TASK_ID}]} --job_num ${SGE_TASK_ID} --log_file $fname --in_feat_dropout 0.5 --dropout 0.4 --batch_size 5
