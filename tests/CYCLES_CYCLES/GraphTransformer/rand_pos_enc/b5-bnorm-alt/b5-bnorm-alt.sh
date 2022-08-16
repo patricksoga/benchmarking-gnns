9bc604e0f3f7890f48f36862279eb4b059ae41ca
patricksoga <psoga@nd.edu>
2022-08-15 22:02:09 -0700

    tests

--- tests/CYCLES_CYCLES/GraphTransformer/rand_pos_enc/b5-bnorm-alt/b5-bnorm-alt.sh
+++ tests/CYCLES_CYCLES/GraphTransformer/rand_pos_enc/b5-bnorm-alt/b5-bnorm-alt.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#$ -N GraphTransformer_CYCLES_b25-lnorm-alt
+#$ -N GraphTransformer_CYCLES_b5-bnorm-alt
 #$ -q gpu
 #$ -l gpu_card=1
-#$ -t 1-1:1
+#$ -t 1-5:1
 
-pos_enc_dim=(0 64)
-fname=$(pwd)/b25-lnorm-alt_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+pos_enc_dim=(0 40 64 128 180 256)
+fname=$(pwd)/b5-bnorm-alt_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_CYCLES_graph_classification.py --config tests/test-configs/GraphTransformer_CYCLES_CYCLES_b25-lnorm-alt.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+python3 main_CYCLES_graph_classification.py --config tests/test-configs/GraphTransformer_CYCLES_CYCLES_b5-bnorm-alt.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
 
 
 # {'dataset': 'CYCLES',
@@ -20,15 +20,15 @@ python3 main_CYCLES_graph_classification.py --config tests/test-configs/GraphTra
 #  'model': 'GraphTransformer',
 #  'net_params': {'L': 10,
 #                 'adj_enc': False,
-#                 'batch_norm': False,
-#                 'batch_size': 25,
+#                 'batch_norm': True,
+#                 'batch_size': 5,
 #                 'dataset': 'CYCLES',
 #                 'dropout': 0.0,
 #                 'full_graph': False,
 #                 'gpu_id': 0,
 #                 'hidden_dim': 80,
 #                 'in_feat_dropout': 0.0,
-#                 'layer_norm': True,
+#                 'layer_norm': False,
 #                 'learned_pos_enc': False,
 #                 'matrix_type': 'A',
 #                 'n_heads': 8,
@@ -42,8 +42,8 @@ python3 main_CYCLES_graph_classification.py --config tests/test-configs/GraphTra
 #                 'residual': True,
 #                 'self_loop': False,
 #                 'wl_pos_enc': False},
-#  'out_dir': 'out/CYCLES_graph_classification_b25-lnorm-alt',
-#  'params': {'batch_size': 25,
+#  'out_dir': 'out/CYCLES_graph_classification_b5-bnorm-alt',
+#  'params': {'batch_size': 5,
 #             'epochs': 1000,
 #             'init_lr': 0.0005,
 #             'lr_reduce_factor': 0.5,
@@ -57,4 +57,4 @@ python3 main_CYCLES_graph_classification.py --config tests/test-configs/GraphTra
 
 
 # Generated with command:
-#python3 configure_tests.py --config ../configs/CYCLES_graph_classification_GraphTransformer_CYCLES_500k.json --batch_size 25 --job_note b25-lnorm-alt --batch_norm False --layer_norm True --rand_pos_enc True --param_values 64
+#python3 configure_tests.py --config ../configs/CYCLES_graph_classification_GraphTransformer_CYCLES_500k.json --batch_size 5 --job_note b5-bnorm-alt --rand_pos_enc True --param_values 40 64 128 180 256 --batch_norm True --layer_norm False
