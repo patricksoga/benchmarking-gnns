b70124c2ae49cd834e0427e165ec73fce962a219
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-08-16 11:18:16 -0400

    results

--- tests/molecules_ZINC/GraphTransformer/rand_pos_enc/b128-bnorm-alt-1e-3/GraphTransformer_ZINC_b128-bnorm-alt-1e-3_16.sh
+++ tests/molecules_ZINC/GraphTransformer/rand_pos_enc/b128-bnorm-alt-1e-3/GraphTransformer_ZINC_b128-bnorm-alt-1e-3_16.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -N GraphTransformer_ZINC_b128-power_iter
+#$ -N GraphTransformer_ZINC_b32-power_iter
 #$ -q gpu@@lalor
 #$ -l gpu_card=1
 
@@ -10,4 +10,4 @@ fsync -d 10 $fname &
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_ZINC_b128-bnorm-alt-1e-3.json --job_num 16 --pos_enc_dim 16 --log_file $fname --power_method True --power_iters 100 --rand_pos_enc False --learned_pos_enc True --batch_size 64 --layer_norm True --batch_norm False
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_ZINC_b128-bnorm-alt-1e-3.json --job_num 16 --pos_enc_dim 16 --log_file $fname --power_method True --power_iters 15 --rand_pos_enc False --learned_pos_enc True --batch_size 32 --layer_norm True --batch_norm False
