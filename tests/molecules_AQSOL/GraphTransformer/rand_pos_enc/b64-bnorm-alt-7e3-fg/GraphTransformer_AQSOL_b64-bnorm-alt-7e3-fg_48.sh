8adf2dfc1820b8a71993b7c4dd7451bc821dd1ca
patricksoga <psoga@nd.edu>
2022-08-16 16:59:51 -0700

    tests

---
+++ tests/molecules_AQSOL/GraphTransformer/rand_pos_enc/b64-bnorm-alt-7e3-fg/GraphTransformer_AQSOL_b64-bnorm-alt-7e3-fg_48.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_AQSOL_b64-bnorm-alt-7e3-fg
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/48_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_AQSOL_b64-bnorm-alt-1e-3-fg.json --job_num 48 --pos_enc_dim 48 --log_file $fname
---
+++ tests/molecules_AQSOL/GraphTransformer/rand_pos_enc/b64-bnorm-alt-7e3-fg/b64-bnorm-alt-7e3-fg.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#$ -N GraphTransformer_AQSOL_b64-bnorm-alt-7e3-fg
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-5:1
+
+pos_enc_dim=(0 16 32 48 64 80)
+fname=$(pwd)/b64-bnorm-alt-1e-3-fg_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_AQSOL_b64-bnorm-alt-1e-3-fg.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+
+
+# {'dataset': 'AQSOL',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'GraphTransformer',
+#  'net_params': {'L': 10,
+#                 'adj_enc': False,
+#                 'batch_norm': True,
+#                 'batch_size': 64,
+#                 'dataset': 'AQSOL',
+#                 'dropout': 0.0,
+#                 'edge_feat': True,
+#                 'full_graph': True,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 64,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': False,
+#                 'learned_pos_enc': False,
+#                 'matrix_type': 'A',
+#                 'n_heads': 8,
+#                 'num_train_data': 7000,
+#                 'out_dim': 64,
+#                 'pos_enc': False,
+#                 'pos_enc_dim': 20,
+#                 'pow_of_mat': 1,
+#                 'rand_pos_enc': True,
+#                 'readout': 'mean',
+#                 'residual': True,
+#                 'self_loop': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/molecules_graph_regression_b64-bnorm-alt-1e-3-fg',
+#  'params': {'batch_size': 64,
+#             'epochs': 1000,
+#             'init_lr': 0.0007,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 10,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'seed': 41,
+#             'weight_decay': 0.0}}
+
+
+
+# Generated with command:
+#python3 configure_tests.py --config ../configs/molecules_graph_regression_GraphTransformer_AQSOL_500k.json --batch_size 64 --job_note b64-bnorm-alt-1e-3-fg --batch_norm True --layer_norm False --rand_pos_enc True --param_values 16 32 48 64 80 --edge_feat True --init_lr 0.0007 --full_graph True --readout mean
