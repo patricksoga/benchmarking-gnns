6c43ec38a87b683398c6ef2243d22550c5131b8a
Patrick Soga <psoga@crcfe02.crc.nd.edu>
2022-08-17 16:43:41 -0400

    results

--- tests/SBMs_SBM_PATTERN/GraphTransformer/rand_pos_enc/b8-lnorm-alt/GraphTransformer_SBM_PATTERN_b8-lnorm-alt_16.sh
+++ tests/SBMs_SBM_PATTERN/GraphTransformer/rand_pos_enc/b8-lnorm-alt/GraphTransformer_SBM_PATTERN_b8-lnorm-alt_16.sh
@@ -10,4 +10,4 @@ fsync -d 10 $fname &
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_SBM_PATTERN_b8-lnorm-alt.json --job_num 16 --pos_enc_dim 16 --log_file $fname
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_SBM_PATTERN_b8-lnorm-alt.json --job_num 16 --pos_enc_dim 16 --log_file $fname --batch_size 4
