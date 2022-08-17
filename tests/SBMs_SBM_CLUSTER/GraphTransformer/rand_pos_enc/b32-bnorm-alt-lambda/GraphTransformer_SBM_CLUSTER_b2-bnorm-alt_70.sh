6609faa0dbc484e593ef6067185a2d41b6b424ad
patricksoga <psoga@nd.edu>
2022-08-17 01:15:50 -0700

    lambda

---
+++ tests/SBMs_SBM_CLUSTER/GraphTransformer/rand_pos_enc/b32-bnorm-alt-lambda/GraphTransformer_SBM_CLUSTER_b2-bnorm-alt_70.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -N GraphTransformer_SBM_CLUSTER_b2-bnorm-alt
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/70_DEBUG.log
+touch $fname
+
+cd; cd benchmarking-gnns;
+
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_SBM_CLUSTER_b2-bnorm-alt.json --job_num 70 --pos_enc_dim 70 --log_file $fname --batch_size 32
