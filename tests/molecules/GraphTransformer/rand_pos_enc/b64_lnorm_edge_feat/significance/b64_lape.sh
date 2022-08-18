447098655fc54ca65c6b5422d011703c63678c15
patricksoga <psoga@nd.edu>
2022-08-17 18:38:33 -0700

    tests

--- tests/molecules/GraphTransformer/rand_pos_enc/b64_lnorm_edge_feat/significance/b64_lape.sh
+++ tests/molecules/GraphTransformer/rand_pos_enc/b64_lnorm_edge_feat/significance/b64_lape.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-#$ -N GraphTransformer_ZINC_b64_lnorm_edge_feat_d128
+#$ -N GraphTransformer_ZINC_b128_bnorm_edge_feat_lape
 #$ -q gpu
 #$ -l gpu_card=1
 #$ -t 1-5:1
 
-# pos_enc_dim=(0 8 16 32 48 64)
-
-pos_enc_dim=(0 16 16 16 16 16)
-fname=$(pwd)/b64_lnorm_edge_feat_d128_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+pos_enc_dim=(0 8 8 8 8 8)
+fname=$(pwd)/b64_bnorm_edge_feat_lape_${SGE_TASK_ID}_DEBUG.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_b128_lnorm_edge_feat_d128.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname --batch_size 64
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_b128_bnorm_edge_feat_lape.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname --batch_size 128 --L 10 --hidden_dim 64 --out_dim 64 --init_lr 0.0007 --batch_norm True --layer_norm False
 
 
 # {'dataset': 'ZINC',
@@ -22,7 +20,7 @@ python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTran
 #  'model': 'GraphTransformer',
 #  'net_params': {'L': 6,
 #                 'adj_enc': False,
-#                 'batch_norm': False,
+#                 'batch_norm': True,
 #                 'batch_size': 128,
 #                 'dataset': 'ZINC',
 #                 'dropout': 0.0,
@@ -31,21 +29,20 @@ python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTran
 #                 'gpu_id': 0,
 #                 'hidden_dim': 80,
 #                 'in_feat_dropout': 0.0,
-#                 'layer_norm': True,
+#                 'layer_norm': False,
 #                 'learned_pos_enc': False,
 #                 'matrix_type': 'A',
 #                 'n_heads': 8,
 #                 'num_train_data': 7000,
 #                 'out_dim': 80,
-#                 'pos_enc': False,
-#                 'pos_enc_dim': 20,
+#                 'pos_enc': True,
+#                 'pos_enc_dim': 8,
 #                 'pow_of_mat': 1,
-#                 'rand_pos_enc': True,
 #                 'readout': 'sum',
 #                 'residual': True,
 #                 'self_loop': False,
 #                 'wl_pos_enc': False},
-#  'out_dir': 'out/molecules_graph_regression_b128_lnorm_edge_feat_d128',
+#  'out_dir': 'out/molecules_graph_regression_b128_bnorm_edge_feat_lape',
 #  'params': {'batch_size': 128,
 #             'epochs': 1000,
 #             'init_lr': 0.001,
