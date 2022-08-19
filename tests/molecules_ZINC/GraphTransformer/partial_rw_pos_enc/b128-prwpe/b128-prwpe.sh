53589befd9a7afe0de97c0e8641b64187fbf5e60
patricksoga <psoga@nd.edu>
2022-08-19 16:13:17 -0700

    tests

---
+++ tests/molecules_ZINC/GraphTransformer/partial_rw_pos_enc/b128-prwpe/b128-prwpe.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#$ -N GraphTransformer_ZINC_b128-prwpe
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-3:1
+
+pos_enc_dim=(0 8 10 20)
+fname=$(pwd)/b128-prwpe_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_ZINC_b128-prwpe.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+
+
+# {'dataset': 'ZINC',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'GraphTransformer',
+#  'net_params': {'L': 10,
+#                 'adj_enc': False,
+#                 'batch_norm': True,
+#                 'batch_size': 128,
+#                 'dataset': 'ZINC',
+#                 'diag': False,
+#                 'dropout': 0.0,
+#                 'edge_feat': True,
+#                 'full_graph': False,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 64,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': False,
+#                 'matrix_type': 'A',
+#                 'n_heads': 8,
+#                 'out_dim': 64,
+#                 'partial_rw_pos_enc': True,
+#                 'pos_enc': False,
+#                 'pos_enc_dim': 8,
+#                 'pow_of_mat': 1,
+#                 'power_method': False,
+#                 'readout': 'mean',
+#                 'residual': True,
+#                 'rw_pos_enc': False,
+#                 'self_loop': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/molecules_graph_regression_b128-prwpe',
+#  'params': {'batch_size': 128,
+#             'epochs': 1000,
+#             'init_lr': 0.0007,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 15,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'save_name': 'b128-prwpe',
+#             'seed': 41,
+#             'seed_array': [41],
+#             'weight_decay': 0.0}}
+
+
+
+# Generated with command:
+#python3 configure_tests.py --config ../configs/molecules_graph_regression_GraphTransformer_ZINC_500k.json --partial_rw_pos_enc True --param_values 8 10 20 --batch_size 128 --job_note b128-prwpe --save_name b128-prwpe --edge_feat True
