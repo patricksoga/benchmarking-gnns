0b08dc20f1a2795b25fc6c7d0c1b8192354fa8b8
patricksoga <psoga@nd.edu>
2022-08-19 13:14:05 -0700

    tests

---
+++ tests/SBMs_SBM_CLUSTER/GraphTransformer/rand_pos_enc/b32-bnorm-alt-ifd.35-d.35/GraphTransformer_SBM_CLUSTER_b32-bnorm-alt-ifd.35-d.35_240.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_SBM_CLUSTER_b32-bnorm-alt-ifd.35-d.35_240
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/240_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_SBM_CLUSTER_b32-bnorm-alt-ifd.35-d.35.json --job_num 240 --pos_enc_dim 240 --log_file $fname
