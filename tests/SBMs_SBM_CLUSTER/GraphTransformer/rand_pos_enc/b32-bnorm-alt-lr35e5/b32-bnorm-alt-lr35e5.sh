f957606441181a9bbc20ef4f8ee176e98b531dd8
patricksoga <psoga@nd.edu>
2022-08-23 07:21:54 -0700

    tests

---
+++ tests/SBMs_SBM_CLUSTER/GraphTransformer/rand_pos_enc/b32-bnorm-alt-lr35e5/b32-bnorm-alt-lr35e5.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#$ -N GraphTransformer_SBM_CLUSTER_b32-bnorm-alt-lr35e5
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-5:1
+
+pos_enc_dim=(0 10 16 32 64 128)
+fname=$(pwd)/b32-bnorm-alt-lr35e5_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_SBM_CLUSTER_b32-bnorm-alt-lr35e5.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+
+
+# {'dataset': 'SBM_CLUSTER',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'GraphTransformer',
+#  'net_params': {'L': 10,
+#                 'adj_enc': False,
+#                 'batch_norm': True,
+#                 'batch_size': 32,
+#                 'dataset': 'SBM_CLUSTER',
+#                 'diag': False,
+#                 'dropout': 0.0,
+#                 'full_graph': False,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 80,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': False,
+#                 'matrix_type': 'A',
+#                 'n_heads': 8,
+#                 'out_dim': 80,
+#                 'partial_rw_pos_enc': False,
+#                 'pos_enc': False,
+#                 'pow_of_mat': 1,
+#                 'power_method': False,
+#                 'rand_pos_enc': True,
+#                 'readout': 'mean',
+#                 'residual': True,
+#                 'rw_pos_enc': False,
+#                 'self_loop': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/SBMs_node_classification_b32-bnorm-alt-lr35e5',
+#  'params': {'batch_size': 32,
+#             'epochs': 1000,
+#             'init_lr': 0.00035,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 10,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'seed': 41,
+#             'seed_array': [41],
+#             'weight_decay': 0.0}}
+
+
+
+# Generated with command:
+#python3 configure_tests.py --config ../configs/SBMs_node_clustering_GraphTransformer_CLUSTER_500k.json --job_note b32-bnorm-alt-lr35e5 --param_values 10 16 32 64 128 --batch_size 32 --rand_pos_enc True --batch_norm True --layer_norm False --partial_rw_pos_enc False --rw_pos_enc False --pos_enc False --init_lr 0.00035
