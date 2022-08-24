b9c2717503c74b6febb023b7b7c656bff787223b
patricksoga <psoga@nd.edu>
2022-08-24 06:47:09 -0700

    tests

--- tests/molecules_ZINC/GraphTransformer/rand_pos_enc/b64-lnorm-full-lr1e3/b64-lnorm-full-lr1e3.sh
+++ tests/molecules_ZINC/GraphTransformer/rand_pos_enc/b64-lnorm-full-lr1e3/b64-lnorm-full-lr1e3.sh
@@ -52,6 +52,7 @@ python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTran
 #             'max_time': 24,
 #             'min_lr': 1e-06,
 #             'print_epoch_interval': 5,
+#             'save_name': 'b64-lnorm-full-lr1e3',
 #             'seed': 41,
 #             'seed_array': [41],
 #             'weight_decay': 0.0}}
@@ -59,4 +60,4 @@ python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTran
 
 
 # Generated with command:
-#python3 configure_tests.py --config ../configs/molecules_graph_regression_GraphTransformer_ZINC_500k.json --batch_size 64 --job_note b64-lnorm-full-lr1e3 --batch_norm False --layer_norm True --rand_pos_enc True --param_values 4 8 16 32 64 --edge_feat True --full_graph True --L 6 --out_dim 80 --hidden_dim 80 --init_lr 0.001
+#python3 configure_tests.py --config ../configs/molecules_graph_regression_GraphTransformer_ZINC_500k.json --batch_size 64 --job_note b64-lnorm-full-lr1e3 --batch_norm False --layer_norm True --rand_pos_enc True --param_values 4 8 16 32 64 --edge_feat True --full_graph True --L 6 --out_dim 80 --hidden_dim 80 --init_lr 0.001 --save_name b64-lnorm-full-lr1e3
