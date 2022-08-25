9fccaf24e6b9bda2ef81d7456f29cb065467d3f3
patricksoga <psoga@nd.edu>
2022-08-24 22:10:43 -0700

    tests

---
+++ tests/molecules_AQSOL/GraphTransformer/pos_enc/b128_bnorm_edge_feat_deep/GraphTransformer_AQSOL_b128_bnorm_edge_feat_deep_12.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_AQSOL_b128_bnorm_edge_feat_deep_12
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/12_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_AQSOL_b128_bnorm_edge_feat_deep.json --job_num 12 --pos_enc_dim 12 --log_file $fname
