1158e5f7e160ed30f366dc71797766b09538b7fa
patricksoga <psoga@nd.edu>
2022-08-26 11:01:51 -0700

    tests

---
+++ tests/SBMs_SBM_PATTERN/GraphTransformer/partial_rw_pos_enc/b26-prwpe/more/GraphTransformer_SBM_PATTERN_b26-prwpe_3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_SBM_PATTERN_b26-prwpe_3
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/3_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_SBM_PATTERN_b26-prwpe.json --job_num 3 --pos_enc_dim 3 --log_file $fname
