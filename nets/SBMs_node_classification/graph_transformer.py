37776d1f0151cfd688c5f2bd772b0eedadf41498
patricksoga <psoga@nd.edu>
2022-08-27 09:51:44 -0700

    update

--- nets/SBMs_node_classification/graph_transformer.py
+++ nets/SBMs_node_classification/graph_transformer.py
@@ -49,14 +49,16 @@ class GraphTransformerNet(nn.Module):
         self.MLP_layer = MLPReadout(out_dim, n_classes)
 
         if self.cat:
-            self.ll = nn.Linear(hidden_dim*2, hidden_dim)
+            # self.ll = nn.Linear(hidden_dim*2, hidden_dim)
+            self.ll = nn.Linear(hidden_dim, hidden_dim - net_params['pos_enc_dim'])
         
     def forward(self, g, h, e, pos_enc=None, h_wl_pos_enc=None):
         h = self.embedding_h(h)
         if self.cat:
+            h = self.ll(h)
             pe = self.pe_layer(g, h, pos_enc)
             h = torch.cat([h, pe], dim=1)
-            h = self.ll(h)
+            # h = self.ll(h)
             h = self.in_feat_dropout(h)
         else:
             h = self.in_feat_dropout(h)
