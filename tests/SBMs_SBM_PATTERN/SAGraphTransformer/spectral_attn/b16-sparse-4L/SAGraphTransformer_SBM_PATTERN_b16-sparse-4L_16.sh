bb251bdc45b70ac03e5f7aa022953a9663ff782d
patricksoga <psoga@nd.edu>
2022-08-27 09:54:43 -0700

    tests

---
+++ tests/SBMs_SBM_PATTERN/SAGraphTransformer/spectral_attn/b16-sparse-4L/SAGraphTransformer_SBM_PATTERN_b16-sparse-4L_16.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N SAGraphTransformer_SBM_PATTERN_b16-sparse-4L_16
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/16_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_SBMs_node_classification.py --config tests/test-configs/SAGraphTransformer_SBMs_SBM_PATTERN_b16-sparse-4L.json --job_num 16 --pos_enc_dim 16 --log_file $fname
