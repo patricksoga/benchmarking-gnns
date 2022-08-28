e9533e42fd5800f0e60da981ba38154423cbc4f9
patricksoga <psoga@nd.edu>
2022-08-28 08:12:07 -0700

    tests

---
+++ tests/CYCLES_CYCLES/SAGraphTransformer/no_pe/b25-noedge/SAGraphTransformer_CYCLES_b25-noedge_8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N SAGraphTransformer_CYCLES_b25-noedge_8
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/8_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_CYCLES_graph_classification.py --config tests/test-configs/SAGraphTransformer_CYCLES_CYCLES_b25-noedge.json --job_num 8 --pos_enc_dim 8 --log_file $fname
---
+++ tests/CYCLES_CYCLES/SAGraphTransformer/no_pe/b25-noedge/b25-noedge.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#$ -N SAGraphTransformer_CYCLES_b25-noedge
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-4:1
+
+pos_enc_dim=(0 8 16 20 32)
+fname=$(pwd)/b25-noedge_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_CYCLES_graph_classification.py --config tests/test-configs/SAGraphTransformer_CYCLES_CYCLES_b25-noedge.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+
+
+# {'dataset': 'CYCLES',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'SAGraphTransformer',
+#  'net_params': {'L': 6,
+#                 'adj_enc': False,
+#                 'batch_norm': True,
+#                 'batch_size': 25,
+#                 'cat_gape': False,
+#                 'dataset': 'CYCLES',
+#                 'diag': False,
+#                 'dropout': 0.0,
+#                 'edge_feat': False,
+#                 'full_graph': False,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 80,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': False,
+#                 'lpe_layers': 3,
+#                 'lpe_n_heads': 4,
+#                 'matrix_type': 'A',
+#                 'n_heads': 10,
+#                 'num_train_data': 200,
+#                 'out_dim': 80,
+#                 'pos_enc': False,
+#                 'pos_enc_dim': 16,
+#                 'pow_of_mat': 1,
+#                 'power_method': False,
+#                 'readout': 'mean',
+#                 'residual': True,
+#                 'rw_pos_enc': False,
+#                 'self_loop': False,
+#                 'spectral_attn': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/CYCLES_graph_classification_b25-noedge',
+#  'params': {'batch_size': 25,
+#             'epochs': 1000,
+#             'init_lr': 0.0005,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 10,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'seed': 41,
+#             'seed_array': [41],
+#             'weight_decay': 0.0}}
+
+
+
+# Generated with command:
+#python3 configure_tests.py --config ../configs/CYCLES_graph_classification_SAGraphTransformer_500k.json --job_note b25-noedge --param_values 8 16 20 32
