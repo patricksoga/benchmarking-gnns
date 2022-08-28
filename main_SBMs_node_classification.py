b09539b8f5f4d62957ed92768965200268eba70e
patricksoga <psoga@nd.edu>
2022-08-27 17:20:27 -0700

    tests

--- main_SBMs_node_classification.py
+++ main_SBMs_node_classification.py
@@ -60,18 +60,18 @@ def train_val_pipeline(MODEL_NAME, dataset, params, net_params, dirs):
             logger.info("[!] Adding graph self-loops for GCN/GAT models (central node trick).")
             dataset._add_self_loops()
 
-    l = 1000
-    dataset.train.graph_lists = dataset.train.graph_lists[:l]
-    dataset.val.graph_lists = dataset.val.graph_lists[:l]
-    dataset.test.graph_lists = dataset.test.graph_lists[:l]
+    # l = 1000
+    # dataset.train.graph_lists = dataset.train.graph_lists[:l]
+    # dataset.val.graph_lists = dataset.val.graph_lists[:l]
+    # dataset.test.graph_lists = dataset.test.graph_lists[:l]
 
-    dataset.train.node_labels = dataset.train.node_labels[:l]
-    dataset.val.node_labels = dataset.val.node_labels[:l]
-    dataset.test.node_labels = dataset.test.node_labels[:l]
+    # dataset.train.node_labels = dataset.train.node_labels[:l]
+    # dataset.val.node_labels = dataset.val.node_labels[:l]
+    # dataset.test.node_labels = dataset.test.node_labels[:l]
 
-    dataset.train.n_samples = len(dataset.train.graph_lists)
-    dataset.val.n_samples = len(dataset.val.graph_lists)
-    dataset.test.n_samples = len(dataset.test.graph_lists)
+    # dataset.train.n_samples = len(dataset.train.graph_lists)
+    # dataset.val.n_samples = len(dataset.val.graph_lists)
+    # dataset.test.n_samples = len(dataset.test.graph_lists)
 
     # train_graphs = []
     # train_labels = []
