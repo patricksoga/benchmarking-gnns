5bcf036066928176667d597b2bc78cb251619573
patricksoga <psoga@nd.edu>
2022-08-29 09:03:03 -0700

    tests

--- tests/CYCLES_CYCLES/GraphTransformer/rand_pos_enc/b25-alt-noedge-ngape4/b25-alt-noedge-ngape4.sh
+++ tests/CYCLES_CYCLES/GraphTransformer/rand_pos_enc/b25-alt-noedge-ngape4/b25-alt-noedge-ngape4.sh
@@ -2,9 +2,9 @@
 #$ -N GraphTransformer_CYCLES_b25-alt-noedge-ngape4
 #$ -q gpu
 #$ -l gpu_card=1
-#$ -t 1-4:1
+#$ -t 1-9:1
 
-pos_enc_dim=(0 26 40 128)
+pos_enc_dim=(0 8 16 26 32 40 56 64 120 128)
 fname=$(pwd)/b25-alt-noedge-ngape4_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
 touch $fname
 fsync -d 10 $fname &
@@ -28,6 +28,7 @@ python3 main_CYCLES_graph_classification.py --config tests/test-configs/GraphTra
 #                 'dropout': 0.0,
 #                 'edge_feat': False,
 #                 'full_graph': False,
+#                 'gape_pooling': 'mean',
 #                 'gpu_id': 0,
 #                 'hidden_dim': 80,
 #                 'in_feat_dropout': 0.0,
@@ -65,4 +66,4 @@ python3 main_CYCLES_graph_classification.py --config tests/test-configs/GraphTra
 
 
 # Generated with command:
-#python3 configure_tests.py --config /home/psoga/Documents/projects/benchmarking-gnns/configs/CYCLES_graph_classification_GraphTransformer_CYCLES_500k.json --job_note b25-alt-noedge-ngape4 --param_values 8 16 32 64 --batch_size 25 --pos_enc False --rand_pos_enc True --edge_feat False --n_gape 4
+#python3 configure_tests.py --config /home/psoga/Documents/projects/benchmarking-gnns/configs/CYCLES_graph_classification_GraphTransformer_CYCLES_500k.json --job_note b25-alt-noedge-ngape4 --param_values 8 16 26 32 40 56 64 120 128 --batch_size 25 --pos_enc False --rand_pos_enc True --edge_feat False --n_gape 4 --gape_pooling mean
