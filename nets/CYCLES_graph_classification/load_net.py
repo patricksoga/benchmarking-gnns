1ae7d1cd96bfff979725b9915bec24ecf1d70970
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-08-31 01:10:59 -0400

    graphormer

--- nets/CYCLES_graph_classification/load_net.py
+++ nets/CYCLES_graph_classification/load_net.py
@@ -7,6 +7,7 @@ from nets.CYCLES_graph_classification.gated_gcn_net import GatedGCNNet
 from nets.CYCLES_graph_classification.gin_net import GINNet
 from nets.CYCLES_graph_classification.graph_transformer import GraphTransformerNet
 from nets.CYCLES_graph_classification.sa_graph_transformer import SAGraphTransformerNet
+from nets.CYCLES_graph_classification.pseudo_graphormer import PseudoGraphormerNet
 
 def GatedGCN(net_params):
     return GatedGCNNet(net_params)
@@ -20,12 +21,16 @@ def GraphTransformer(net_params):
 def SAGraphTransformer(net_params):
     return SAGraphTransformerNet(net_params)
 
+def PseudoGraphormer(net_params):
+    return PseudoGraphormerNet(net_params)
+
 def gnn_model(MODEL_NAME, net_params):
     models = {
         'GatedGCN': GatedGCN,
         'GIN': GIN,
         'GraphTransformer': GraphTransformer,
-        'SAGraphTransformer': SAGraphTransformer
+        'SAGraphTransformer': SAGraphTransformer,
+        'PseudoGraphormer': PseudoGraphormer
     }
         
     return models[MODEL_NAME](net_params)
