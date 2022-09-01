54782716ebea524a942c40a3a9d8a2002d994783
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-01 14:05:31 -0400

    tests

--- tests/molecules_ZINC/GraphTransformer/rand_pos_enc/b128-bnorm-alt-noedge-500k/b128-bnorm-alt-noedge-500k.sh
+++ tests/molecules_ZINC/GraphTransformer/rand_pos_enc/b128-bnorm-alt-noedge-500k/b128-bnorm-alt-noedge-500k.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-#$ -N GraphTransformer_AQSOL_b128_bnorm_noedge_rand
+#$ -N GraphTransformer_ZINC_b128-bnorm-alt-noedge-500k
 #$ -q gpu
 #$ -l gpu_card=1
-#$ -t 1-6:1
+#$ -t 1-30:1
 
-pos_enc_dim=(0 8 10 12 16 32 64)
-fname=$(pwd)/b128_bnorm_noedge_rand_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+pos_enc_dim=(0 4 6 12 18 20 22 24 26 28 30 34 38 40 42 44 46 48 50 54 56 58 60 62 170 172 174 176 178 180 182)
+fname=$(pwd)/b128-bnorm-alt-noedge-500k_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_AQSOL_b128_bnorm_noedge_rand.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_ZINC_b128-bnorm-alt-noedge-500k.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
 
 
-# {'dataset': 'AQSOL',
+# {'dataset': 'ZINC',
 #  'gpu': {'id': 0, 'use': True},
 #  'model': 'GraphTransformer',
 #  'net_params': {'L': 10,
 #                 'adj_enc': False,
-#                 'batch_norm': False,
+#                 'batch_norm': True,
 #                 'batch_size': 128,
 #                 'cat_gape': False,
-#                 'dataset': 'AQSOL',
+#                 'dataset': 'ZINC',
 #                 'diag': False,
 #                 'dropout': 0.0,
 #                 'edge_feat': False,
@@ -31,32 +31,30 @@ python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTran
 #                 'gpu_id': 0,
 #                 'hidden_dim': 80,
 #                 'in_feat_dropout': 0.0,
-#                 'layer_norm': True,
-#                 'learned_pos_enc': False,
+#                 'layer_norm': False,
 #                 'matrix_type': 'A',
 #                 'n_heads': 8,
-#                 'num_train_data': 7000,
 #                 'out_dim': 80,
-#                 'pos_enc': False,
-#                 'pos_enc_dim': 20,
+#                 'pos_enc_dim': 8,
 #                 'pow_of_mat': 1,
 #                 'power_method': False,
 #                 'rand_pos_enc': True,
-#                 'readout': 'sum',
+#                 'readout': 'mean',
 #                 'residual': True,
 #                 'rw_pos_enc': False,
 #                 'self_loop': False,
 #                 'spectral_attn': False,
 #                 'wl_pos_enc': False},
-#  'out_dir': 'out/molecules_graph_regression_b128_bnorm_noedge_rand',
+#  'out_dir': 'out/molecules_graph_regression_b128-bnorm-alt-noedge-500k',
 #  'params': {'batch_size': 128,
 #             'epochs': 1000,
 #             'init_lr': 0.0007,
 #             'lr_reduce_factor': 0.5,
-#             'lr_schedule_patience': 10,
+#             'lr_schedule_patience': 15,
 #             'max_time': 24,
 #             'min_lr': 1e-06,
 #             'print_epoch_interval': 5,
+#             'save_name': 'b128-bnorm-alt-noedge-500k',
 #             'seed': 41,
 #             'seed_array': [41],
 #             'weight_decay': 0.0}}
@@ -64,4 +62,4 @@ python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTran
 
 
 # Generated with command:
-#python3 configure_tests.py --config test-configs/GraphTransformer_molecules_b64_lnorm_edge_feat_rand.json --varying_param pos_enc_dim --param_values 8 10 12 16 32 64 --batch_size 128 --init_lr 0.0007 --job_note b128_bnorm_noedge_rand --rand_pos_enc True --rand_pos_enc True --pos_enc False --hidden_dim 80 --out_dim 80 --L 10 --edge_feat False
+#python3 configure_tests.py --config ../configs/molecules_graph_regression_GraphTransformer_ZINC_500k.json --batch_size 128 --job_note b128-bnorm-alt-noedge-500k --batch_norm True --layer_norm False --rand_pos_enc True --param_values 4 8 16 32 64 --edge_feat False --init_lr 0.0007 --save_name b128-bnorm-alt-noedge-500k --L 10 --hidden_dim 80 --out_dim 80
