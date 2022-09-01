9c8f2ebc31e71de1155aaf6220789cf9fbd881ec
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-01 04:08:32 -0400

    fix

--- data/SBMs.py
+++ data/SBMs.py
@@ -217,7 +217,8 @@ class SBMsDataset(torch.utils.data.Dataset):
         #tab_snorm_e = [ torch.FloatTensor(size,1).fill_(1./float(size)) for size in tab_sizes_e ]
         #snorm_e = torch.cat(tab_snorm_e).sqrt()
         batched_graph = dgl.batch(graphs)
-        if all(bool(x) for x in spatial_pos_biases):
+        # if all(bool(x) for x in spatial_pos_biases):
+        if all([x is not None for x in spatial_pos_biases]):
             batched_spatial_pos_biases = torch.block_diag(*spatial_pos_biases)
         else:
             batched_spatial_pos_biases = None
