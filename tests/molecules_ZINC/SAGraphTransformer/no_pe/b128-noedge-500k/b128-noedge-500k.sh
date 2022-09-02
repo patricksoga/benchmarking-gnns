3c3642013c7edab8e6a33df200a496bab935e6e0
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-01 20:53:53 -0400

    tests

--- tests/molecules_ZINC/SAGraphTransformer/no_pe/b128-noedge-500k/b128-noedge-500k.sh
+++ tests/molecules_ZINC/SAGraphTransformer/no_pe/b128-noedge-500k/b128-noedge-500k.sh
@@ -4,7 +4,7 @@
 #$ -l gpu_card=1
 #$ -t 1-1:1
 
-pos_enc_dim=(0 8)
+pos_enc_dim=(0 4 8 16 32)
 fname=$(pwd)/b128-noedge-500k_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
 touch $fname
 fsync -d 10 $fname &
