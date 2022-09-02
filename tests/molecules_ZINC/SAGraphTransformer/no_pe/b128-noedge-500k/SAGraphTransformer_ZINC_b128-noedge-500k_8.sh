29cecdb27f280e92972224b43373064f6d1ce651
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-01 23:51:33 -0400

    tests

--- tests/molecules_ZINC/SAGraphTransformer/no_pe/b128-noedge-500k/SAGraphTransformer_ZINC_b128-noedge-500k_8.sh
+++ tests/molecules_ZINC/SAGraphTransformer/no_pe/b128-noedge-500k/SAGraphTransformer_ZINC_b128-noedge-500k_8.sh
@@ -3,11 +3,11 @@
 #$ -q gpu
 #$ -l gpu_card=1
 
-fname=$(pwd)/8_DEBUG_3lpe.log
+fname=$(pwd)/8_DEBUG_4lpe.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_molecules_graph_regression.py --config tests/test-configs/SAGraphTransformer_molecules_ZINC_b128-noedge-500k.json --job_num 8 --pos_enc_dim 8 --log_file $fname --lpe_layers 3
+python3 main_molecules_graph_regression.py --config tests/test-configs/SAGraphTransformer_molecules_ZINC_b128-noedge-500k.json --job_num 8 --pos_enc_dim 8 --log_file $fname --lpe_layers 4
