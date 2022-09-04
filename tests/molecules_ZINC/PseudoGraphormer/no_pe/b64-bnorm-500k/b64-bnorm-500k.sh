39ae995e95bdf84e3df7d050ea53610ede8ab7b3
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-03 20:46:19 -0400

    tests

---
+++ tests/molecules_ZINC/PseudoGraphormer/no_pe/b64-bnorm-500k/b64-bnorm-500k.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#$ -N PseudoGraphormer_ZINC_b64-bnorm-500k
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-1:1
+
+pos_enc_dim=(0 1)
+fname=$(pwd)/b64-bnorm-500k_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_molecules_graph_regression.py --config tests/test-configs/PseudoGraphormer_molecules_ZINC_b64-bnorm-500k.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+
+
+# {'dataset': 'ZINC',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'PseudoGraphormer',
+#  'net_params': {'L': 12,
+#                 'adj_enc': False,
+#                 'batch_norm': True,
+#                 'batch_size': 64,
+#                 'cat_gape': False,
+#                 'dataset': 'ZINC',
+#                 'diag': False,
+#                 'dropout': 0.0,
+#                 'full_graph': False,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 80,
+#                 'in_deg_centrality': 64,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': False,
+#                 'matrix_type': 'A',
+#                 'n_heads': 8,
+#                 'out_deg_centrality': 64,
+#                 'out_dim': 80,
+#                 'pos_enc': False,
+#                 'pow_of_mat': 1,
+#                 'power_method': False,
+#                 'readout': 'mean',
+#                 'residual': True,
+#                 'rw_pos_enc': False,
+#                 'self_loop': False,
+#                 'spd_len': 128,
+#                 'spectral_attn': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/molecules_graph_regression_b64-bnorm-500k',
+#  'params': {'batch_size': 64,
+#             'epochs': 1000,
+#             'init_lr': 0.0007,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 10,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'seed': 41,
+#             'seed_array': [41],
+#             'weight_decay': 0.0}}
+
+
+
+# Generated with command:
+#python3 configure_tests.py --config ../configs/molecules_graph_regression_PseudoGraphormer_ZINC_500k.json --job_note b64-bnorm-500k --param_values 1 --batch_size 64 --L 12
