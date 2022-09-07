c5d861fc3e7b7f2f26c4d7919848f1e7ef5f84bd
patricksoga <psoga@nd.edu>
2022-09-07 12:45:35 -0700

    tests

---
+++ tests/molecules_ZINC/GraphTransformer/rand_pos_enc/b128-bnorm-alt-500k-noedge-fg-study/GraphTransformer_ZINC_b128-bnorm-alt-500k-noedge-fg-study_16.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_ZINC_b128-bnorm-alt-500k-noedge-fg-study_16
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/16_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_ZINC_b128-bnorm-alt-500k-noedge-fg-study.json --job_num 16 --pos_enc_dim 16 --log_file $fname
---
+++ tests/molecules_ZINC/GraphTransformer/rand_pos_enc/b128-bnorm-alt-noedge-fg-study/all.sh
@@ -0,0 +1,9 @@
+pos_enc_dim=(2 4 8 16 32 64 128 256)
+curr_dir=$(pwd)
+
+for i in ${pos_enc_dim[@]}; 
+do 
+    cd ~/Documents/projects/benchmarking-gnns;
+    fname=${curr_dir}/b128-bnorm-alt-noedge-fg-study_${i}_${pos_enc_dim[${i}]}_DEBUG.log;
+    python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_ZINC_b128-bnorm-alt-noedge-fg-study.json --job_num ${i} --pos_enc_dim ${i} --log_file $fname --full_graph False
+done
