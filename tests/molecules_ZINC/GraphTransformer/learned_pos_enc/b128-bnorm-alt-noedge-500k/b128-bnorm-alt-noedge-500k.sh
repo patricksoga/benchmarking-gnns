a7f771dd6ee30b9dbb19b28e7a888924551b5278
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-09 22:07:59 -0400

    tests

--- tests/molecules_ZINC/GraphTransformer/learned_pos_enc/b128-bnorm-alt-noedge-500k/b128-bnorm-alt-noedge-500k.sh
+++ tests/molecules_ZINC/GraphTransformer/learned_pos_enc/b128-bnorm-alt-noedge-500k/b128-bnorm-alt-noedge-500k.sh
@@ -4,7 +4,7 @@
 #$ -l gpu_card=1
 #$ -t 1-1:1
 
-pos_enc_dim=(0 16)
+pos_enc_dim=(0 8)
 fname=$(pwd)/b128-bnorm-alt-noedge-500k_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
 touch $fname
 fsync -d 10 $fname &
@@ -66,4 +66,4 @@ python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTran
 
 
 # Generated with command:
-#python3 configure_tests.py --config ../configs/molecules_graph_regression_GraphTransformer_ZINC_500k.json --batch_size 128 --job_note b128-bnorm-alt-noedge-500k --batch_norm True --layer_norm False --rand_pos_enc False --learned_pos_enc True --param_values 16 --edge_feat False --init_lr 0.0007 --save_name b128-bnorm-alt-noedge-500k --L 10 --hidden_dim 80 --out_dim 80
+#python3 configure_tests.py --config ../configs/molecules_graph_regression_GraphTransformer_ZINC_500k.json --batch_size 128 --job_note b128-bnorm-alt-noedge-500k --batch_norm True --layer_norm False --rand_pos_enc False --learned_pos_enc True --param_values 8 --edge_feat False --init_lr 0.0007 --save_name b128-bnorm-alt-noedge-500k --L 10 --hidden_dim 80 --out_dim 80
