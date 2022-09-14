c20f085dcf3f60366894309054c673795f562334
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-14 17:44:24 -0400

    tests

--- tests/SBMs_SBM_PATTERN/SAGraphTransformer/spectral_attn/b26-500k-sparse-trials/b26-500k-sparse-trials.sh
+++ tests/SBMs_SBM_PATTERN/SAGraphTransformer/spectral_attn/b26-500k-sparse-trials/b26-500k-sparse-trials.sh
@@ -18,7 +18,7 @@ python3 main_SBMs_node_classification.py --config tests/test-configs/SAGraphTran
 # {'dataset': 'SBM_PATTERN',
 #  'gpu': {'id': 0, 'use': True},
 #  'model': 'SAGraphTransformer',
-#  'net_params': {'L': 4,
+#  'net_params': {'L': 6,
 #                 'adj_enc': False,
 #                 'batch_norm': True,
 #                 'batch_size': 26,
@@ -26,6 +26,7 @@ python3 main_SBMs_node_classification.py --config tests/test-configs/SAGraphTran
 #                 'dataset': 'SBM_PATTERN',
 #                 'diag': False,
 #                 'dropout': 0.0,
+#                 'eigen_bartels_stewart': False,
 #                 'full_graph': False,
 #                 'gape_clamp': False,
 #                 'gape_individual': False,
@@ -46,6 +47,7 @@ python3 main_SBMs_node_classification.py --config tests/test-configs/SAGraphTran
 #                 'pow_of_mat': 1,
 #                 'power_method': False,
 #                 'rand_pos_enc': False,
+#                 'rand_sketchy_pos_enc': False,
 #                 'random_orientation': False,
 #                 'readout': 'mean',
 #                 'residual': True,
@@ -69,4 +71,4 @@ python3 main_SBMs_node_classification.py --config tests/test-configs/SAGraphTran
 
 
 # Generated with command:
-#python3 configure_tests.py --config ../configs/SBMs_node_clustering_SAGraphTransformer_PATTERN_500k.json --job_note b26-500k-sparse-trials --spectral_attn True --pos_enc False --rand_pos_enc False --param_values 10 --full_graph True --L 4 --full_graph False --batch_size 26 --lpe_dim 16 --seed_array 41 95 22 35
+#python3 configure_tests.py --config ../configs/SBMs_node_clustering_SAGraphTransformer_PATTERN_500k.json --job_note b26-500k-sparse-trials --spectral_attn True --pos_enc False --rand_pos_enc False --param_values 10 --full_graph True --L 6 --full_graph False --batch_size 26 --lpe_dim 16 --seed_array 41 95 22 35
