284dae067600082f64245249eebb75e00917b3de
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-14 17:25:21 -0400

    tests

--- tests/SBMs_SBM_CLUSTER/GraphTransformer/learned_pos_enc/b16-bnorm-clamped/b16-bnorm-clamped.sh
+++ tests/SBMs_SBM_CLUSTER/GraphTransformer/learned_pos_enc/b16-bnorm-clamped/b16-bnorm-clamped.sh
@@ -1,50 +1,46 @@
 #!/bin/bash
-#$ -N GraphTransformer_AQSOL_b128-bnorm-noedge-500k
+#$ -N GraphTransformer_SBM_CLUSTER_b16-bnorm-clamped
 #$ -q gpu
 #$ -l gpu_card=1
-#$ -t 1-3:1
+#$ -t 1-5:1
 
-pos_enc_dim=(0 8 16 20)
-fname=$(pwd)/b128-bnorm-noedge-500k_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+pos_enc_dim=(0 8 10 12 14 16)
+fname=$(pwd)/b16-bnorm-clamped_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
 touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_AQSOL_b128-bnorm-noedge-500k.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_SBM_CLUSTER_b16-bnorm-clamped.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
 
 
-# {'dataset': 'AQSOL',
+# {'dataset': 'SBM_CLUSTER',
 #  'gpu': {'id': 0, 'use': True},
 #  'model': 'GraphTransformer',
 #  'net_params': {'L': 10,
 #                 'adj_enc': False,
-#                 'batch_norm': False,
-#                 'batch_size': 128,
+#                 'batch_norm': True,
+#                 'batch_size': 16,
 #                 'cat_gape': False,
-#                 'dataset': 'AQSOL',
+#                 'dataset': 'SBM_CLUSTER',
 #                 'diag': False,
 #                 'dropout': 0.0,
-#                 'edge_feat': False,
 #                 'eigen_bartels_stewart': False,
 #                 'full_graph': False,
-#                 'gape_clamp': False,
+#                 'gape_clamp': True,
 #                 'gape_individual': False,
 #                 'gape_softmax_after': False,
 #                 'gape_softmax_before': False,
 #                 'gpu_id': 0,
 #                 'hidden_dim': 80,
 #                 'in_feat_dropout': 0.0,
-#                 'layer_norm': True,
-#                 'learned_pos_enc': False,
+#                 'layer_norm': False,
+#                 'learned_pos_enc': True,
 #                 'matrix_type': 'A',
 #                 'n_heads': 8,
-#                 'num_train_data': 7000,
 #                 'out_dim': 80,
-#                 'partial_rw_pos_enc': True,
 #                 'pos_enc': False,
-#                 'pos_enc_dim': 20,
 #                 'pow_of_mat': 1,
 #                 'power_method': False,
 #                 'rand_pos_enc': False,
@@ -56,16 +52,15 @@ python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTran
 #                 'self_loop': False,
 #                 'spectral_attn': False,
 #                 'wl_pos_enc': False},
-#  'out_dir': 'out/molecules_graph_regression_b128-bnorm-noedge-500k',
-#  'params': {'batch_size': 128,
+#  'out_dir': 'out/SBMs_node_classification_b16-bnorm-clamped',
+#  'params': {'batch_size': 16,
 #             'epochs': 1000,
-#             'init_lr': 0.0007,
+#             'init_lr': 0.0005,
 #             'lr_reduce_factor': 0.5,
 #             'lr_schedule_patience': 10,
-#             'max_time': 24,
+#             'max_time': 64.0,
 #             'min_lr': 1e-06,
 #             'print_epoch_interval': 5,
-#             'save_name': 'b128_bnorm_noedge-500k',
 #             'seed': 41,
 #             'seed_array': [41],
 #             'weight_decay': 0.0}}
@@ -73,4 +68,4 @@ python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTran
 
 
 # Generated with command:
-#python3 configure_tests.py --config test-configs/GraphTransformer_molecules_b64_lnorm_edge_feat_rand.json --varying_param pos_enc_dim --param_values 8 16 20 --batch_size 128 --init_lr 0.0007 --pos_enc False --partial_rw_pos_enc True --rand_pos_enc False --hidden_dim 80 --out_dim 80 --readout mean --L 10 --save_name b128_bnorm_noedge-500k --job_note b128-bnorm-noedge-500k --edge_feat False
+#python3 configure_tests.py --config ../configs/SBMs_node_clustering_GraphTransformer_CLUSTER_500k.json --batch_size 16 --learned_pos_enc True --rand_pos_enc False --param_values 8 10 12 14 16 --job_note b16-bnorm-clamped --batch_norm True --layer_norm False --pos_enc False --gape_clamp True --max_time 64
