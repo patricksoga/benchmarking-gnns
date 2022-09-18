cb5ee895bf41cb9d2be9efd8979f3662a05308d4
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-18 00:24:17 -0400

    tests

---
+++ tests/SBMs_SBM_PATTERN/GraphTransformer/rand_pos_enc/b26-bnorm-alt-clamp-ngape4-4-ind-trials/GraphTransformer_SBM_PATTERN_b26-bnorm-alt-clamp-ngape4-4-ind-trials_41_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_SBM_PATTERN_b26-bnorm-alt-clamp-ngape4-4-ind-trials_4_0_41
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/4_DEBUG_0_41.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_SBM_PATTERN_b26-bnorm-alt-clamp-ngape4-4-ind-trials.json --job_num 4 --pos_enc_dim 4 --log_file $fname --seed_array 41
