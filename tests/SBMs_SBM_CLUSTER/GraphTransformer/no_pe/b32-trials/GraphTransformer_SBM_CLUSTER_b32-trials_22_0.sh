c8b02b0b51aac7e40d8945996cbae5dc53a5d051
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-20 19:46:08 -0400

    tests

---
+++ tests/SBMs_SBM_CLUSTER/GraphTransformer/no_pe/b32-trials/GraphTransformer_SBM_CLUSTER_b32-trials_22_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_SBM_CLUSTER_b32-trials_10_0_22
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/10_DEBUG_0_22.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransformer_SBMs_SBM_CLUSTER_b32-trials.json --job_num 10 --pos_enc_dim 10 --log_file $fname --seed_array 22
