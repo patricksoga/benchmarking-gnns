38b2f8642329b2373ea12418e54c1492071cb484
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-20 19:43:27 -0400

    tests

---
+++ tests/CSL_CSL/PseudoGraphormer/no_pe/b5-spd-300k-trials/PseudoGraphormer_CSL_b5-spd-300k-trials_22_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N PseudoGraphormer_CSL_b5-spd-300k-trials_32_0_22
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/32_DEBUG_0_22.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_CSL_graph_classification.py --config tests/test-configs/PseudoGraphormer_CSL_CSL_b5-spd-300k-trials.json --job_num 32 --pos_enc_dim 32 --log_file $fname --seed_array 22
---
+++ tests/CSL_CSL/PseudoGraphormer/no_pe/b5-spd-300k-trials/b5-spd-300k-trials.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#$ -N PseudoGraphormer_CSL_b5-spd-300k-trials
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-1:1
+
+pos_enc_dim=(0 32)
+fname=$(pwd)/b5-spd-300k-trials_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_CSL_graph_classification.py --config tests/test-configs/PseudoGraphormer_CSL_CSL_b5-spd-300k-trials.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+
+
+# {'dataset': 'CSL',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'PseudoGraphormer',
+#  'net_params': {'L': 6,
+#                 'adj_enc': False,
+#                 'batch_norm': True,
+#                 'batch_size': 5,
+#                 'cat_gape': False,
+#                 'dataset': 'CSL',
+#                 'diag': False,
+#                 'dropout': 0.0,
+#                 'eigen_bartels_stewart': False,
+#                 'full_graph': False,
+#                 'gape_clamp': False,
+#                 'gape_individual': False,
+#                 'gape_softmax_after': False,
+#                 'gape_softmax_before': False,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 80,
+#                 'in_deg_centrality': 64,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': False,
+#                 'matrix_type': 'A',
+#                 'n_heads': 8,
+#                 'out_deg_centrality': 64,
+#                 'out_dim': 80,
+#                 'pos_enc': False,
+#                 'pow_of_mat': 1,
+#                 'power_method': False,
+#                 'rand_pos_enc': False,
+#                 'rand_sketchy_pos_enc': False,
+#                 'random_orientation': False,
+#                 'readout': 'mean',
+#                 'residual': True,
+#                 'rw_pos_enc': False,
+#                 'self_loop': False,
+#                 'spd_len': 128,
+#                 'spectral_attn': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/CSL_graph_classification_b5-spd-300k-trials',
+#  'params': {'batch_size': 5,
+#             'epochs': 1000,
+#             'init_lr': 0.0005,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 10,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'seed': 41,
+#             'seed_array': [41, 95, 22, 35],
+#             'weight_decay': 0.0}}
+
+
+
+# Generated with command:
+#python3 configure_tests.py --config ../configs/CSL_graph_classification_PseudoGraphormer_500k.json --rand_pos_enc False --pos_enc False --rand_pos_enc False --param_values 32 --job_note b5-spd-300k-trials --seed_array 41 95 22 35 --L 6
