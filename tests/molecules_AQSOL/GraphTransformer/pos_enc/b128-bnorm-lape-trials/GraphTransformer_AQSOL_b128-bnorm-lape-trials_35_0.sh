3caa9afd5bd414453417ea226317581f5a7bed50
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-22 05:59:24 -0400

    tests

---
+++ tests/molecules_AQSOL/GraphTransformer/pos_enc/b128-bnorm-lape-trials/GraphTransformer_AQSOL_b128-bnorm-lape-trials_35_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_AQSOL_b128-bnorm-lape-trials_8_0_35
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/8_DEBUG_0_35.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_molecules_graph_regression.py --config tests/test-configs/GraphTransformer_molecules_AQSOL_b128-bnorm-lape-trials.json --job_num 8 --pos_enc_dim 8 --log_file $fname --seed_array 35
