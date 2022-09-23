13715a873cbc647ae1c504515e32e7aff4092e69
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-22 21:23:51 -0400

    tests

---
+++ tests/Planarity_Planarity/PseudoGraphormer/no_pe/b32-pseudo-500k-fg-trials/PseudoGraphormer_Planarity_b32-pseudo-500k-fg-trials_35_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N PseudoGraphormer_Planarity_b32-pseudo-500k-fg-trials_1_0_35
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/1_DEBUG_0_35.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_Planarity_graph_classification.py --config tests/test-configs/PseudoGraphormer_Planarity_Planarity_b32-pseudo-500k-fg-trials.json --job_num 1 --pos_enc_dim 1 --log_file $fname --seed_array 35
