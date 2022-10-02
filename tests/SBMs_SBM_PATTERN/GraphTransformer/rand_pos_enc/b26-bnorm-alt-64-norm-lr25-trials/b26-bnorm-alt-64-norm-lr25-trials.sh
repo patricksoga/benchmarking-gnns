234babf74461737f262e421e67dcc837caf6aac1
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-10-02 14:09:10 -0400

    tests

--- tests/SBMs_SBM_PATTERN/GraphTransformer/rand_pos_enc/b26-bnorm-alt-64-norm-lr25-trials/b26-bnorm-alt-64-norm-lr25-trials.sh
+++ tests/SBMs_SBM_PATTERN/GraphTransformer/rand_pos_enc/b26-bnorm-alt-64-norm-lr25-trials/b26-bnorm-alt-64-norm-lr25-trials.sh
@@ -39,6 +39,7 @@ python3 main_SBMs_node_classification.py --config tests/test-configs/GraphTransf
 #                 'gape_softmax_before': False,
 #                 'gape_squash': 'none',
 #                 'gape_symmetric': False,
+#                 'gape_weight_gen': False,
 #                 'gpu_id': 0,
 #                 'hidden_dim': 80,
 #                 'in_feat_dropout': 0.0,
