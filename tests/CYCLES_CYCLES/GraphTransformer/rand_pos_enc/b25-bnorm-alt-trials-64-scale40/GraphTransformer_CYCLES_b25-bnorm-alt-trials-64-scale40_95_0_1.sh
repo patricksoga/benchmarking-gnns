d27cb253c2fbaac3a421d0f334e46970db4cf28f
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-10-05 03:01:25 -0400

    tests

---
+++ tests/CYCLES_CYCLES/GraphTransformer/rand_pos_enc/b25-bnorm-alt-trials-64-scale40/GraphTransformer_CYCLES_b25-bnorm-alt-trials-64-scale40_95_0_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_CYCLES_b25-bnorm-alt-trials-64-scale40_64_0_95
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/64_DEBUG_0_95.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_CYCLES_graph_classification.py --config tests/test-configs/GraphTransformer_CYCLES_CYCLES_b25-bnorm-alt-trials-64-scale40.json --job_num 64 --pos_enc_dim 64 --log_file $fname --seed_array 95
