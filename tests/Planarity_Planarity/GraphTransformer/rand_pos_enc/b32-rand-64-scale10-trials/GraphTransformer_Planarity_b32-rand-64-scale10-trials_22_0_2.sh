9d26ef2546ba2496fac650bd2eee7a92beff8c50
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-10-06 12:11:16 -0400

    tests

---
+++ tests/Planarity_Planarity/GraphTransformer/rand_pos_enc/b32-rand-64-scale10-trials/GraphTransformer_Planarity_b32-rand-64-scale10-trials_22_0_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_Planarity_b32-rand-64-scale10-trials_64_0_22
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/64_DEBUG_0_22.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_Planarity_graph_classification.py --config tests/test-configs/GraphTransformer_Planarity_Planarity_b32-rand-64-scale10-trials.json --job_num 64 --pos_enc_dim 64 --log_file $fname --seed_array 22
