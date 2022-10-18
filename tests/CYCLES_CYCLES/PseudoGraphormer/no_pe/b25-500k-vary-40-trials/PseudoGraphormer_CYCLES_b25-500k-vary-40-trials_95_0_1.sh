82a11b1089164f124968807b81cde59021ebd7a1
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-10-18 03:20:03 -0400

    tests

---
+++ tests/CYCLES_CYCLES/PseudoGraphormer/no_pe/b25-500k-vary-40-trials/PseudoGraphormer_CYCLES_b25-500k-vary-40-trials_95_0_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N PseudoGraphormer_CYCLES_b25-500k-vary-40-trials_1_0_95
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/1_DEBUG_0_95.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_CYCLES_graph_classification.py --config tests/test-configs/PseudoGraphormer_CYCLES_CYCLES_b25-500k-vary-40-trials.json --job_num 1 --pos_enc_dim 1 --log_file $fname --seed_array 95
