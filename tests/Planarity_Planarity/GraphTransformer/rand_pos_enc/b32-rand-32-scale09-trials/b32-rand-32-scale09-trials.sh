7cc251d58178b9fae2e9ef3356fe2aa015366308
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-10-19 17:25:01 -0400

    tests

---
+++ tests/Planarity_Planarity/GraphTransformer/rand_pos_enc/b32-rand-32-scale09-trials/b32-rand-32-scale09-trials.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#$ -N GraphTransformer_Planarity_b32-rand-32-scale09-trials
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-1:1
+
+pos_enc_dim=(0 32)
+fname=$(pwd)/b32-rand-32-scale09-trials_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_Planarity_graph_classification.py --config tests/test-configs/GraphTransformer_Planarity_Planarity_b32-rand-32-scale09-trials.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+
+
+# {'dataset': 'Planarity',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'GraphTransformer',
+#  'net_params': {'L': 10,
+#                 'adj_enc': False,
+#                 'batch_norm': True,
+#                 'batch_size': 32,
+#                 'cat_gape': False,
+#                 'cycles_k': 6,
+#                 'dataset': 'Planarity',
+#                 'diag': False,
+#                 'dropout': 0.0,
+#                 'eigen_bartels_stewart': False,
+#                 'experiment_1': False,
+#                 'full_graph': False,
+#                 'gape_clamp': False,
+#                 'gape_div': False,
+#                 'gape_individual': False,
+#                 'gape_norm': False,
+#                 'gape_normalization': False,
+#                 'gape_rand': False,
+#                 'gape_scale': '0.9',
+#                 'gape_softmax_after': False,
+#                 'gape_softmax_before': False,
+#                 'gape_squash': 'none',
+#                 'gape_symmetric': False,
+#                 'gape_weight_gen': False,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 80,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': False,
+#                 'matrix_type': 'A',
+#                 'n_heads': 8,
+#                 'num_train_data': 7000,
+#                 'out_dim': 80,
+#                 'pos_enc': False,
+#                 'pos_enc_dim': 20,
+#                 'pow_of_mat': 1,
+#                 'power_method': False,
+#                 'rand_pos_enc': True,
+#                 'rand_sketchy_pos_enc': False,
+#                 'random_orientation': False,
+#                 'readout': 'mean',
+#                 'residual': True,
+#                 'rw_pos_enc': False,
+#                 'self_loop': False,
+#                 'spectral_attn': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/Planarity_graph_classification_b32-rand-32-scale09-trials',
+#  'params': {'batch_size': 32,
+#             'epochs': 1000,
+#             'init_lr': 0.0005,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 10,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'seed': 41,
+#             'seed_array': [41, 95, 22, 35],
+#             'weight_decay': 0.0}}
+
+
+
+# Generated with command:
+#python3 configure_tests.py --config ../configs/PLANARITY_graph_classification_GraphTransformer_500k_sparse_graph_BN.json --job_note b32-rand-32-scale09-trials --seed_array 41 95 22 35 --param_values 32 --rand_pos_enc True --pos_enc False --batch_size 32 --gape_clamp False --gape_scale 0.9
