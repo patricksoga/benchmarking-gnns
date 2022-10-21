bb5987669b9c4341742c5ef344a412d5a7b9831a
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-10-21 11:06:24 -0400

    tests

--- tests/CSL_CSL/GraphTransformer/rand_pos_enc/b5-300k-rand/GraphTransformer_CSL_b5-300k-rand_35_0_3.sh
+++ tests/CSL_CSL/GraphTransformer/rand_pos_enc/b5-300k-rand/GraphTransformer_CSL_b5-300k-rand_35_0_3.sh
@@ -10,4 +10,4 @@ fsync -d 10 $fname &
 conda activate gnn
 cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
 
-python3 main_CSL_graph_classification.py --config tests/test-configs/GraphTransformer_CSL_CSL_b5-300k-rand.json --job_num 32 --pos_enc_dim 32 --log_file $fname --seed_array 35
+python3 main_CSL_graph_classification.py --config tests/test-configs/GraphTransformer_CSL_rand_enc_b5.json --job_num 32 --pos_enc_dim 32 --log_file $fname --seed_array 35
