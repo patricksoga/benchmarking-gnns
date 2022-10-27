11ebdbea35faa90001a6b369b083373a31070639
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-10-27 02:19:12 -0400

    tests

---
+++ tests/CYCLES_CYCLES/SAGraphTransformer/no_pe/b25-noedge-500k-lpe1-lpedim8-vary2-trials/SAGraphTransformer_CYCLES_b25-noedge-500k-lpe1-lpedim8-vary2-trials_41_0_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N SAGraphTransformer_CYCLES_b25-noedge-500k-lpe1-lpedim8-vary2-trials_20_0_41
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/20_DEBUG_0_41.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_CYCLES_graph_classification.py --config tests/test-configs/SAGraphTransformer_CYCLES_CYCLES_b25-noedge-500k-lpe1-lpedim8-vary2-trials.json --job_num 20 --pos_enc_dim 20 --log_file $fname --seed_array 41
