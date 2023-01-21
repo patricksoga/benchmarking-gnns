e873409706346c7c54a018c8b67754c12e7d572d
psoga <psoga@nd.edu>
2023-01-20 18:22:23 -0800

    sa ogb

---
+++ tests/OGB_OGB/SAGraphTransformer/no_pe/gt_ogb_b1024-baseline-noedge/SAGraphTransformer_OGB_gt_ogb_b1024-baseline-noedge_95_0_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N SAGraphTransformer_OGB_gt_ogb_b1024-baseline-noedge_10_0_95
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/10_DEBUG_0_95.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_OGB_graph_regression.py --config tests/test-configs/SAGraphTransformer_OGB_OGB_gt_ogb_b1024-baseline-noedge.json --job_num 10 --pos_enc_dim 10 --log_file $fname --seed_array 95
