c65c2f69dc03a23bb82eb8ffdb3ef2a87997a183
psoga <psoga@nd.edu>
2023-01-23 03:45:48 -0800

    ogb gape stoch softinit initials100 topn

---
+++ tests/OGB_OGB/GraphTransformer/rand_pos_enc/b1024-bnorm-alt-noedge-500k-32-stoch-softinit-initials100-topn-trials/b1024-bnorm-alt-noedge-500k-32-stoch-softinit-initials100-topn-trials.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#$ -N GraphTransformer_OGB_b1024-bnorm-alt-noedge-500k-32-stoch-softinit-initials100-topn-trials
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-1:1
+
+pos_enc_dim=(0 32)
+fname=$(pwd)/b1024-bnorm-alt-noedge-500k-32-stoch-softinit-initials100-topn-trials_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_OGB_graph_regression.py --config tests/test-configs/GraphTransformer_OGB_OGB_b1024-bnorm-alt-noedge-500k-32-stoch-softinit-initials100-topn-trials.json --job_num ${SGE_TASK_ID} --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]} --log_file $fname
+
+
+# {'dataset': 'OGB',
+#  'gpu': {'id': 0, 'use': True},
+#  'model': 'GraphTransformer',
+#  'net_params': {'L': 10,
+#                 'adj_enc': False,
+#                 'batch_norm': True,
+#                 'batch_size': 1024,
+#                 'cat_gape': False,
+#                 'cycles_k': 6,
+#                 'dataset': 'OGB',
+#                 'diag': False,
+#                 'dropout': 0.0,
+#                 'edge_feat': False,
+#                 'eigen_bartels_stewart': False,
+#                 'experiment_1': False,
+#                 'full_graph': False,
+#                 'gape_beta': 1.0,
+#                 'gape_break_batch': False,
+#                 'gape_clamp': False,
+#                 'gape_cond_lbl': False,
+#                 'gape_div': False,
+#                 'gape_individual': False,
+#                 'gape_norm': False,
+#                 'gape_normalization': False,
+#                 'gape_normalize_mat': False,
+#                 'gape_per_layer': False,
+#                 'gape_rand': False,
+#                 'gape_scalar': False,
+#                 'gape_scale': '0',
+#                 'gape_softmax_after': False,
+#                 'gape_softmax_before': False,
+#                 'gape_softmax_init': True,
+#                 'gape_squash': 'none',
+#                 'gape_stack_strat': '1',
+#                 'gape_stoch': True,
+#                 'gape_symmetric': False,
+#                 'gape_tau': False,
+#                 'gape_tau_mat': False,
+#                 'gape_uniform_init': False,
+#                 'gape_weight_gen': False,
+#                 'gape_weight_id': False,
+#                 'gpu_id': 0,
+#                 'hidden_dim': 80,
+#                 'in_feat_dropout': 0.0,
+#                 'layer_norm': False,
+#                 'matrix_type': 'A',
+#                 'n_heads': 8,
+#                 'ngape_agg': 'sum',
+#                 'ngape_betas': [],
+#                 'num_initials': 100,
+#                 'out_dim': 80,
+#                 'partial_rw_pos_enc': False,
+#                 'pos_enc_dim': 8,
+#                 'pow_of_mat': 1,
+#                 'power_method': False,
+#                 'rand_pos_enc': True,
+#                 'rand_sketchy_pos_enc': False,
+#                 'random_orientation': False,
+#                 'readout': 'mean',
+#                 'residual': True,
+#                 'rw_pos_enc': False,
+#                 'self_loop': False,
+#                 'spectral_attn': False,
+#                 'wl_pos_enc': False},
+#  'out_dir': 'out/OGB_graph_regression_b1024-bnorm-alt-noedge-500k-32-stoch-softinit-initials100-topn-trials',
+#  'params': {'batch_size': 1024,
+#             'epochs': 1000,
+#             'init_lr': 0.0007,
+#             'lr_reduce_factor': 0.5,
+#             'lr_schedule_patience': 15,
+#             'max_time': 24,
+#             'min_lr': 1e-06,
+#             'print_epoch_interval': 5,
+#             'save_name': 'b1024-bnorm-alt-noedge-500k-32-stoch-softinit-initials100-topn-trials',
+#             'seed': 41,
+#             'seed_array': [41, 95, 22, 35],
+#             'weight_decay': 0.0}}
+
+
+
+# Generated with command:
+#python3 configure_tests.py --config ../configs/molecules_graph_regression_GraphTransformer_ZINC_500k.json --batch_size 1024 --job_note b1024-bnorm-alt-noedge-500k-32-stoch-softinit-initials100-topn-trials --batch_norm True --layer_norm False --rand_pos_enc True --param_values 32 --seed_array 41 95 22 35 --edge_feat False --init_lr 0.0007 --save_name b1024-bnorm-alt-noedge-500k-32-stoch-softinit-initials100-topn-trials --L 10 --hidden_dim 80 --out_dim 80 --gape_clamp False --gape_stoch True --gape_softmax_init True --num_initials 100 --gape_stack_strat 1 --dataset OGB
