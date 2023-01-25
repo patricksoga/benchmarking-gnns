7815d6b2d84ad25ffe0ce34a61eb732dd65fdd6d
psoga <psoga@nd.edu>
2023-01-25 02:04:24 -0800

    stoch ogb

---
+++ tests/OGB_OGB/GraphTransformer/rand_pos_enc/b1024-bnorm-alt-noedge-500k-32-stoch-trials/GraphTransformer_OGB_b1024-bnorm-alt-noedge-500k-32-stoch-trials_41_0_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GraphTransformer_OGB_b1024-bnorm-alt-noedge-500k-32-stoch-trials_32_0_41
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/32_DEBUG_0_41.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+
+python3 main_OGB_graph_regression.py --config tests/test-configs/GraphTransformer_OGB_OGB_b1024-bnorm-alt-noedge-500k-32-stoch-trials.json --job_num 32 --pos_enc_dim 32 --log_file $fname --seed_array 41
